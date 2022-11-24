25458f6c26f71bd3bacd668db52a766e5401015b
ZnakeDenmark <Victor.dyhrman@gmail.com>
2022-11-24 23:56:19 +0100

    Tilføjet CSS og Javascript filerne

---
+++ app/static/game.js
@@ -0,0 +1,64 @@
+var point = 1;
+var upgradeExtra = 1;
+var upg1cost = 10;
+var upg2cost = 100;
+var autopoint = 0;
+
+function main()
+{
+    document.getElementById("perclick").innerHTML = "Per click " + upgradeExtra;
+    point = point + upgradeExtra;
+    document.getElementById("points").innerHTML = "Points: " + Math.floor(point); 
+    document.getElementById("upgrade1").innerHTML = "upgrade: per click stat - price: " + Math.floor(upg1cost)
+
+}
+
+
+function upgrade()
+{
+    if (point >= upg1cost)
+    {
+    upgradeExtra = upgradeExtra + 1;
+    point = point - upg1cost
+    upg1cost = upg1cost * 1.1
+    document.getElementById("points").innerHTML = "Points: " + Math.floor(point); 
+    document.getElementById("perclick").innerHTML = "Per click " + Math.floor(upgradeExtra);
+    document.getElementById("upgrade1").innerHTML = "upgrade: per click stat - price: " + Math.floor(upg1cost)
+    }else{
+        document.getElementById('error').innerHTML = "Error:you dont have enough for that"
+        setTimeout(() => {document.getElementById('error').innerHTML = ""}, 3000);
+
+    }
+    
+}
+
+
+function autoupgrade()
+{
+    if (point >= upg2cost){
+    point = point - upg2cost;
+    autopoint = 1 + autopoint;
+    upg2cost = upg2cost * 1.15;
+    document.getElementById("upgrade2").innerHTML = "upgrade: auto income - price: " + Math.floor(upg2cost);
+    update();
+    }else{
+        document.getElementById('error').innerHTML = "Error: you dont have enough for that"
+        setTimeout(() => {document.getElementById('error').innerHTML = ""}, 3000);
+    }
+    
+}
+
+setInterval(function update()
+{
+        console.log("Du burde få 1 per sekund")
+        console.log(autopoint)
+        console.log(point = point + autopoint)
+        document.getElementById("points").innerHTML = "Points: " + Math.floor(point); 
+        document.getElementById("prSecond").innerHTML = "Per second: " + autopoint;
+       
+}, 1000);
+
+
+autoupgrade();
+upgrade();
+main();
