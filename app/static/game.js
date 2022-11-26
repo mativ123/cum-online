0724a823057c5fff0d186850098f7a7b07817ae9
mathias <selandermathias00@gmail.com>
2022-11-26 22:22:47 +0100

    restrukturering af game.js

--- app/static/game.js
+++ app/static/game.js
@@ -6,6 +6,7 @@ var upgrades = {
         per: 1,
         baseprice: 10,
         pricescale: 1.1,
+        button: "click1",
     },
 
     click2: {
@@ -13,6 +14,7 @@ var upgrades = {
         per: 100,
         baseprice: 100,
         pricescale: 1.1,
+        button: "click2",
     },
 
     auto1: {
@@ -20,6 +22,7 @@ var upgrades = {
         per: 1,
         baseprice: 100,
         pricescale: 1.2,
+        button: "auto1",
     },
 
     auto2: {
@@ -27,6 +30,7 @@ var upgrades = {
         per: 10,
         baseprice: 3000,
         pricescale: 1.5,
+        button: "auto2",
     },
 };
 
@@ -34,14 +38,46 @@ var clickList = [upgrades.click1, upgrades.click2];
 var autoList = [upgrades.auto1, upgrades.auto2];
 
 const calcPrice = (upgrade) => upgrade.baseprice * Math.pow(upgrade.pricescale, upgrade.n);
-const calcPer = (obj) => {
+
+function calcPer(obj)
+{
     var res = 0;
     for(var i = 0; i<obj.length; i += 1)
     {
         res += obj[i].n * obj[i].per;
     }
     return res;
-};
+}
+
+function upClick(clicked)
+{
+    if(point >= calcPrice(clicked))
+    {
+        point -= calcPrice(clicked);
+        clicked.n += 1;
+        document.getElementById("perclick").innerHTML = `Per click: ${calcPer(clickList) + 1}`;
+        document.getElementById(clicked.button).innerHTML = `upgrade: per click stat - price: ${Math.floor(calcPrice(clicked))}`;
+    } else
+    {
+        document.getElementById('error').innerHTML = "Error:you dont have enough for that"
+        setTimeout(() => {document.getElementById('error').innerHTML = ""}, 3000);
+    }
+}
+
+function upAuto(clicked)
+{
+    if(point >= calcPrice(clicked))
+    {
+        point -= calcPrice(clicked);
+        clicked.n += 1;
+        document.getElementById("prSecond").innerHTML = `Per second: ${calcPer(autoList)}`;
+        document.getElementById(clicked.button).innerHTML = `upgrade: auto income - price: ${Math.floor(calcPrice(clicked))}`;
+    } else
+    {
+        document.getElementById('error').innerHTML = "Error:you dont have enough for that"
+        setTimeout(() => {document.getElementById('error').innerHTML = ""}, 3000);
+    }
+}
 
 function main()
 {
@@ -54,60 +90,16 @@ function upgrade(n)
     switch(n)
     {
         case 0:
-            current = upgrades.click1;
-            if(point >= calcPrice(current))
-            {
-                point -= calcPrice(current);
-                current.n += 1;
-                document.getElementById("perclick").innerHTML = `Per click: ${calcPer(clickList) + 1}`;
-                document.getElementById("click1").innerHTML = `upgrade: per click stat - price: ${Math.floor(calcPrice(current))}`;
-            } else
-            {
-                document.getElementById('error').innerHTML = "Error:you dont have enough for that"
-                setTimeout(() => {document.getElementById('error').innerHTML = ""}, 3000);
-            }
+            upClick(upgrades.click1);
             break;
         case 1:
-            current = upgrades.click2;
-            if(point >= calcPrice(current))
-            {
-                point -= calcPrice(current);
-                current.n += 1;
-                document.getElementById("perclick").innerHTML = `Per click: ${calcPer(clickList) + 1}`;
-                document.getElementById("click2").innerHTML = `upgrade: per click stat - price: ${Math.floor(calcPrice(current))}`;
-            } else
-            {
-                document.getElementById('error').innerHTML = "Error:you dont have enough for that"
-                setTimeout(() => {document.getElementById('error').innerHTML = ""}, 3000);
-            }
+            upClick(upgrade.click2);
             break;
         case 2:
-            current = upgrades.auto1;
-            if(point >= calcPrice(current))
-            {
-                point -= calcPrice(current);
-                current.n += 1;
-                document.getElementById("prSecond").innerHTML = `Per second: ${calcPer(autoList)}`;
-                document.getElementById("auto1").innerHTML = `upgrade: auto income - price: ${Math.floor(calcPrice(current))}`;
-            } else
-            {
-                document.getElementById('error').innerHTML = "Error:you dont have enough for that"
-                setTimeout(() => {document.getElementById('error').innerHTML = ""}, 3000);
-            }
+            upAuto(upgrades.auto1);
             break;
         case 3:
-            current = upgrades.auto2;
-            if(point >= calcPrice(current))
-            {
-                point -= calcPrice(current);
-                current.n += 1;
-                document.getElementById("prSecond").innerHTML = `Per second: ${calcPer(autoList)}`;
-                document.getElementById("auto2").innerHTML = `upgrade: auto income - price: ${Math.floor(calcPrice(current))}`;
-            } else
-            {
-                document.getElementById('error').innerHTML = "Error:you dont have enough for that"
-                setTimeout(() => {document.getElementById('error').innerHTML = ""}, 3000);
-            }
+            upAuto(upgrades.auto2);
             break;
         default:
             console.log('not cock');
